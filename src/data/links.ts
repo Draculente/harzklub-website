6296fcd12b053a338ae9a2a5e302b71777edb3a8
Draculente <me@draculente.eu>
2022-12-30 18:08:15 +0100

    :sparkles: add under construction page

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_AUFGABEN_SECTION_LINK = "/aktuelles";
-export const MITMACHEN = "#";
-export const UNDER_CONSTRUCTION = "#";
+export const UNDER_CONSTRUCTION = "/im-aufbau";
+export const DEFAULT_AUFGABEN_SECTION_LINK = UNDER_CONSTRUCTION;
+export const MITMACHEN = UNDER_CONSTRUCTION;
 export const IMPRESSUM = "/impressum";
