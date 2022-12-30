fab40d9ee44bc034c5a143ec3f7de96c23546df4
Draculente <me@draculente.eu>
2022-12-30 18:57:41 +0100

    :art: small improvements

--- src/data/links.ts
+++ src/data/links.ts
@@ -3,3 +3,4 @@ export const DEFAULT_AUFGABEN_SECTION_LINK = UNDER_CONSTRUCTION;
 export const MITMACHEN = UNDER_CONSTRUCTION;
 export const IMPRESSUM = "/impressum";
 export const DOWNLOADS = "/downloads";
+export const ORG_LOGO_LINK = "https://harzklub.de/";
