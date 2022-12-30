856b3a7bd287c59347283203bbc5cbb2374e3ec8
Draculente <me@draculente.eu>
2022-12-30 18:41:05 +0100

    :sparkles: add downloads page. add download button

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,3 +2,4 @@ export const UNDER_CONSTRUCTION = "/im-aufbau";
 export const DEFAULT_AUFGABEN_SECTION_LINK = UNDER_CONSTRUCTION;
 export const MITMACHEN = UNDER_CONSTRUCTION;
 export const IMPRESSUM = "/impressum";
+export const DOWNLOADS = "/downloads";
