1c380543aa99f25dd8a3d973ecbdbebfa0d6b529
Draculente <me@draculente.eu>
2023-02-10 13:02:30 +0100

    upgrade astro and other dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://harzklub-hohegeiss.de"
+  site: "https://harzklub-hohegeiss.de/"
 });
